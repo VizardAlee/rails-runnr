ab480bc8a9ff02236987dee81138f971e1f7e4ef
Aliyu <biggy0023@gmail.com>
2024-05-06 18:37:31 +0100

    corrected some database stuff

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,5 +1,5 @@
 require 'paystack'
-require 'dotenv/load'
+# require 'dotenv/load'
 
 class CheckoutsController < ApplicationController
 
@@ -16,48 +16,48 @@ class CheckoutsController < ApplicationController
     end 
 
     begin
-      line_items = cart.map do |item| # Use the cart array from set_cart
-        product = Product.find(item["id"])
-        product_stock = product.product_stocks.find{ |ps| ps.size ==  item["size"].to_i}
+      # line_items = cart.map do |item| # Use the cart array from set_cart
+      #   product = Product.find(item["id"])
+      #   product_stock = product.product_stocks.find{ |ps| ps.size ==  item["size"].to_i}
 
-        if product_stock.quantity < item["quantity"].to_i
-          render json: { error: "Not enough stock for #{product.name}"},  status: 400
-          return
-        end
-        {
-          name: product.name,
-          amount: product.price * 100, # Paystack expects amount in kobo
-          quantity: item["quantity"].to_i ,
-          metadata: { product_id: product.id, size: item["size"].to_i, product_stock_id: product_stock.id}
-        }
-      end
+      #   if product_stock.quantity < item["quantity"].to_i
+      #     render json: { error: "Not enough stock for #{product.name}"},  status: 400
+      #     return
+      #   end
+      #   {
+      #     name: product.name,
+      #     amount: product.price * 100, # Paystack expects amount in kobo
+      #     quantity: item["quantity"].to_i ,
+      #     metadata: { product_id: product.id, size: item["size"].to_i, product_stock_id: product_stock.id}
+      #   }
+      # end
 
-      paystack = Paystack.new(ENV['PAYSTACK_PUBLIC_KEY'], ENV['PAYSTACK_PRIVATE_KEY'])
-      logger.debug("Paystack Secret Key: #{ENV['PAYSTACK_SECRET_KEY']}") # Add this line
-      logger.debug("Paystack Object Created: #{paystack.inspect}")
-      transactions = PaystackTransactions.new(paystack)
+    #   paystack = Paystack.new(ENV['PAYSTACK_PUBLIC_KEY'], ENV['PAYSTACK_PRIVATE_KEY'])
+    #   logger.debug("Paystack Secret Key: #{ENV['PAYSTACK_SECRET_KEY']}") # Add this line
+    #   logger.debug("Paystack Object Created: #{paystack.inspect}")
+    #   transactions = PaystackTransactions.new(paystack)
 
-      payment_response = PaystackTransactions.initializeTransaction(paystack,
-      reference: generate_reference, 
-      # email: current_admin.email, 
-      amount: calculate_total(line_items) * 100, 
-       metadata: { cart: line_items },
-      callback_url: success_callback 
-    ) 
-      logger.debug("Paystack response status: #{payment_response.status}")
+    #   payment_response = PaystackTransactions.initializeTransaction(paystack,
+    #   reference: generate_reference, 
+    #   # email: current_admin.email, 
+    #   amount: calculate_total(line_items) * 100, 
+    #    metadata: { cart: line_items },
+    #   callback_url: success_callback 
+    # )
+      # logger.debug("Paystack response status: #{payment_response.status}")
 
 
-      if payment_response.status
-        redirect_to payment_response.authorization_url
-      else
-        logger.debug("Paystack initialization failed, response: #{payment_response.inspect}")
-        raise "Paystack Error: #{payment_response.inspect}" 
-      end
+      # if payment_response.status
+      #   redirect_to payment_response.authorization_url
+      # else
+      #   logger.debug("Paystack initialization failed, response: #{payment_response.inspect}")
+      #   raise "Paystack Error: #{payment_response.inspect}" 
+      # end
 
     rescue => error
-      logger.error "Error during Paystack checkout: #{error.class} - #{error.message}"
-      logger.error "Paystack API Response: #{error.response.inspect}" if error.respond_to?(:response)
-      render json: { error: "An error occurred while processing your payment" }, status: 500
+      # logger.error "Error during Paystack checkout: #{error.class} - #{error.message}"
+      # logger.error "Paystack API Response: #{error.response.inspect}" if error.respond_to?(:response)
+      # render json: { error: "An error occurred while processing your payment" }, status: 500
       # render json: { status: 'success' } # Replace with your desired JSON response 
       # redirect_to cart_path, alert: "An error occurred while processing your payment."
       # logger.error "Paystack Error: #{error.class} - #{error.message}" 
@@ -65,30 +65,30 @@ class CheckoutsController < ApplicationController
   end
 
   def success_callback
-    RestClient.ssl_ca_file = File.join(Rails.root, 'config', 'certificates', 'cacert.pem') 
-    # 1. Verify payment reference with Paystack
-    transaction_ref = params[:trxref]
-    paystack = Paystack.new(ENV['PAYSTACK_PUBLIC_KEY'], ENV['PAYSTACK_PRIVATE_KEY']) 
-    # transactions = PaystackTransactions.new(paystack) 
-    response = PaystackTransactions.verify(paystack, transaction_ref) 
+    # RestClient.ssl_ca_file = File.join(Rails.root, 'config', 'certificates', 'cacert.pem') 
+    # # 1. Verify payment reference with Paystack
+    # transaction_ref = params[:trxref]
+    # paystack = Paystack.new(ENV['PAYSTACK_PUBLIC_KEY'], ENV['PAYSTACK_PRIVATE_KEY']) 
+    # # transactions = PaystackTransactions.new(paystack) 
+    # response = PaystackTransactions.verify(paystack, transaction_ref) 
 
-    if response.status
-    # Find the corresponding order
-    order = Order.find_by(transaction_reference: transaction_ref) 
-      if order
-        order.update(status: :paid)
-        # ... update inventory based on order.metadata  
-        current_user.cart.clear  # Assuming 'current_user' represents the logged-in user
-        redirect_to order_confirmation_path(order), notice: "Your transaction was successful!  Your cart has been cleared."
-      else
-        # Handle case where the corresponding order is not found
-        logger.error "Order not found for transaction reference: #{transaction_ref}"
-        redirect_to root_path, alert: "Payment verification error. Please contact support."
-      end
-    else
-      # Handle unsuccessful payment
-      redirect_to orders_path, alert: "Payment failed. Please try again."
-    end
+    # if response.status
+    # # Find the corresponding order
+    # order = Order.find_by(transaction_reference: transaction_ref) 
+    #   if order
+    #     order.update(status: :paid)
+    #     # ... update inventory based on order.metadata  
+    #     current_user.cart.clear  # Assuming 'current_user' represents the logged-in user
+    #     redirect_to order_confirmation_path(order), notice: "Your transaction was successful!  Your cart has been cleared."
+    #   else
+    #     # Handle case where the corresponding order is not found
+    #     logger.error "Order not found for transaction reference: #{transaction_ref}"
+    #     redirect_to root_path, alert: "Payment verification error. Please contact support."
+    #   end
+    # else
+    #   # Handle unsuccessful payment
+    #   redirect_to orders_path, alert: "Payment failed. Please try again."
+    # end
   end
 
   def success
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_20_134107) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,15 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_134107) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "admin_product_stocks", force: :cascade do |t|
-    t.integer "size"
-    t.integer "amount"
-    t.integer "product_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_admin_product_stocks_on_product_id"
-  end
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -68,8 +62,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_134107) do
   end
 
   create_table "order_products", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.integer "order_id", null: false
+    t.bigint "product_id", null: false
+    t.bigint "order_id", null: false
     t.string "size"
     t.integer "quantity"
     t.datetime "created_at", null: false
@@ -88,7 +82,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_134107) do
   end
 
   create_table "product_stocks", force: :cascade do |t|
-    t.integer "product_id", null: false
+    t.bigint "product_id", null: false
     t.integer "size"
     t.integer "quantity"
     t.datetime "created_at", null: false
@@ -98,20 +92,29 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_134107) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.text "description"
+    t.string "image"
     t.integer "price"
-    t.integer "category_id", null: false
-    t.boolean "active"
+    t.string "description"
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "admin_product_stocks", "products"
   add_foreign_key "order_products", "orders"
   add_foreign_key "order_products", "products"
   add_foreign_key "product_stocks", "products"
-  add_foreign_key "products", "categories"
 end
